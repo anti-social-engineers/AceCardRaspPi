8606e11d1f0ddabec13925e455516b38e7255aaa
Ralph <0896320@hr.nl>
2019-05-21 10:50:55 +0200

    update to write

--- AceCardScanner/AceWrite.py
+++ AceCardScanner/AceWrite.py
@@ -26,7 +26,9 @@ if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
     print('Error! Failed to authenticate block 4 with the card.')
     sys.exit(-1)
 
-data = bytearray("Hello world", 'ascii')
+data = bytearray(16)
+data[0:4] = b'MCPI'
+data[4] = 'Hello world' & 0xFF
 
 if not pn532.mifare_classic_write_block(4, data):
     print('Error! Failed to write to the card.')
