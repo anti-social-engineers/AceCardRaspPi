04853101cf250d4d25d70b53940211a12d459830
Ralph <0896320@hr.nl>
2019-05-21 13:59:19 +0200

    .

--- AceCardScanner/test.py
+++ AceCardScanner/test.py
@@ -1,9 +1,16 @@
 import binascii
+# a='1234567891234567891234567891234a'
+# print(type(a))
+# s=binascii.unhexlify(a)
+# print(type(s))
+# b= bytearray(s)
+# print(type(b))
+# t = len(b)
+# print(t)
+#
+# l = binascii.hexlify(b)
+#
+# print(l)
 
-print("test")
-data = bytearray('bbbbbbbbbbbbbbbb', 'utf-8')
-print(data)
-print(len(data))
-
-print(data.decode('utf-8'))
-
+test = binascii.hexlify('test')
+print(test)
