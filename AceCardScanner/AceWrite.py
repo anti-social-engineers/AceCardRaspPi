757500903c8a19c48335af32582680bf31ed6e9a
Ralph <0896320@hr.nl>
2019-05-21 10:03:47 +0200

    Added prints to notify scanner where it is

--- AceCardScanner/AceWrite.py
+++ AceCardScanner/AceWrite.py
@@ -13,22 +13,24 @@ pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
 pn532.begin()
 pn532.SAM_configuration()
 
+print("Place card on the the scanner to write")
 uid = pn532.read_passive_target()
 while uid is None:
     uid = pn532.read_passive_target()
 print(uid)
 print(binascii.hexlify(uid))
 
+print("Authenticating...")
 if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
                                                CARD_KEY):
     print('Error! Failed to authenticate block 4 with the card.')
     sys.exit(-1)
 
+"Starting writing "
 data = "hello world"
 if not pn532.mifare_classic_write_block(4, data):
     print('Error! Failed to write to the card.')
     sys.exit(-1)
-print("writing succesfull")
+print("Writing succesfull")
 
-test = "test"
-print(":DWAesqwadecfs")
+
