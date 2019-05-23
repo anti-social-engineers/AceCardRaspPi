f2c2a283a1e170e2c9bc50da0fd4ae1a5dbd4cd6
Ralph <0896320@hr.nl>
2019-05-23 09:27:08 +0200

    new read and write version

---
+++ AceCardScanner/acewrite.py
@@ -0,0 +1,51 @@
+import binascii
+import sys
+
+import Adafruit_PN532 as PN532
+
+# PN532 configuration for a Raspberry Pi:
+CS   = 18
+MOSI = 23
+MISO = 24
+SCLK = 25
+
+# Configure the key to use for writing to the MiFare card.  You probably don't
+# need to change this from the default below unless you know your card has a
+# different key associated with it.
+CARD_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+# Create and initialize an instance of the PN532 class.
+pn532 =  PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
+pn532.begin()
+pn532.SAM_configuration()
+
+block_blacklist = [0, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63]
+
+print("Waiting for card...")
+uid = None
+while uid is None:
+    uid = pn532.read_passive_target()
+print("Found card with UID: {0}",format(binascii.hexlify(uid)))
+print('==============================================================')
+print('WARNING: DO NOT REMOVE CARD FROM PN532 UNTIL FINISHED WRITING!')
+print('==============================================================')
+
+block = int(input("Which block"))
+if block in block_blacklist:
+    print("Not allowed to write in sector trailers!")
+else:
+    if not pn532.mifare_classic_authenticate_block(uid, block, PN532.MIFARE_CMD_AUTH_B,
+                                               CARD_KEY):
+        print('Error! Failed to authenticate block with the card.')
+        sys.exit(-1)
+
+    data ='1234567891234567891234567891234a'
+    dataHex =binascii.unhexlify(data)
+    dataArray = bytearray(dataHex, 'utf-8')
+    if not pn532.mifare_classic_write_block(block, dataArray):
+        print('Error! Failed to write to the card.')
+        sys.exit(-1)
+
+
+
+
--- AceCardScanner/test.py
+++ AceCardScanner/test.py
@@ -1,16 +1,26 @@
 import binascii
 # a='1234567891234567891234567891234a'
 # print(type(a))
-# s=binascii.unhexlify(a)
-# print(type(s))
-# b= bytearray(s)
+# # s=binascii.unhexlify(a)
+#
+# b= bytearray(a, 'utf-16')
 # print(type(b))
 # t = len(b)
 # print(t)
+#
+# value = format('hello')
+#
 #
 # l = binascii.hexlify(b)
 #
 # print(l)
 
-test = binascii.hexlify('test')
-print(test)
+
+data = '123456789123456789A23C5678912345'
+dataHex = binascii.unhexlify(data)
+dataArray = bytearray(dataHex)
+print(len(dataArray))
+print(dataArray)
+
+l = binascii.hexlify(dataArray)
+print(l)
