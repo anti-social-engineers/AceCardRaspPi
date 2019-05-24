fa5f25d26856ff8eea3a468a7a883c801c1d7e27
mikekeehnen <mikekeehnen@me.com>
2019-05-24 12:15:27 +0200

    New test file with CardContent

--- PiServer/func/aceread.py
+++ PiServer/func/aceread.py
@@ -15,27 +15,30 @@ def Read():
   pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
   key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
   pn532.begin()
-
+  CardContent = ""
   ic, ver, rev, support = pn532.get_firmware_version()
-  return('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+  CardContent += (('Found PN532 with firmware version: {0}.{1}'.format(ver, rev)) + "\n")
 
 
   while True:
       uid = pn532.read_passive_target()
       if uid is not None:
-          return('Found card with UID: {0}'.format((binascii.hexlify(uid))))
-          block = int(input("Which block to read?"))
-          if not pn532.mifare_classic_authenticate_block(uid, block, PN532.MIFARE_CMD_AUTH_B,
-                                                         key):
-              return('Failed to authenticate block {0}!'.format(block))
-              continue
-          else:
-              data = pn532.mifare_classic_read_block(block)
-              if data is None:
-                  return('Failed to read block {0}!'.format(block))
-                  continue
-              else:
-                  return('Reading block {0}: {1}'.format(block, binascii.hexlify(data)))
+          CardContent += (('Found card with UID: {0}'.format((binascii.hexlify(uid)))) + "\n")
+          blocks = {5, 10, 12, 18, 20, 25, 30, 37, 41, 46, 48, 54, 57, 62}
+          
+          for block in blocks:
+            if not pn532.mifare_classic_authenticate_block(uid, block, PN532.MIFARE_CMD_AUTH_B,
+                                                           key):
+                CardContent += (('Failed to authenticate block {0}!'.format(block)) + "\n")
+                continue
+            else:
+                data = pn532.mifare_classic_read_block(block)
+                if data is None:
+                    CardContent += (('Failed to read block {0}!'.format(block)) + "\n")
+                    continue
+                else:
+                    CardContent += (('Reading block {0}: {1}'.format(block, binascii.hexlify(data))) + "\n")
+            return CardContent
       else:
           continue
 
