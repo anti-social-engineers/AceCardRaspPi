1d680b2dff5094109610b7c0676c88816dabdb31
Ralph <0896320@hr.nl>
2019-05-27 10:34:23 +0200

    fix in key.py

--- key.py
+++ key.py
@@ -4,11 +4,13 @@ CARD_KEY_B = [0x75, 0x42, 0x64, 0x35, 0x5f, 0x5d]
 CARD_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
 
 def getkey():
-    choice = input("which key? a/b/default")
+    choice = input("which key? a/b/d(default)")
     if choice.lower() == "a":
         return CARD_KEY_A
     elif choice.lower() == "b":
         return CARD_KEY_B
-    else:
+    elif choice.lower() == "d":
         return CARD_KEY
+    return CARD_KEY
+
 
