fe4411ea7b9b8741b003864f4f4afd21095e3d4d
mikekeehnen <mikekeehnen@me.com>
2019-05-29 15:37:50 +0200

    test

--- PiServer/app.py
+++ PiServer/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+import sys
+
+sys.path.insert(0, '~/projects/AceCardRaspPi/func')
+
+import test
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/aceread')
+def Read():
+   return test
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
