50315fe6f5d876d9f1bff03b85a6243a7d72bb36
Ralph <0896320@hr.nl>
2019-06-05 09:59:59 +0200

    fix in keypadtest

--- Hardware/KeyPadTest.py
+++ Hardware/KeyPadTest.py
@@ -6,10 +6,10 @@ from Hardware import adafruit_matrixkeypad
 cols = [DigitalInOut(x) for x in (board.D9, board.D6, board.D5)]
 rows = [DigitalInOut(x) for x in (board.D13, board.D12, board.D11, board.D10)]
 
-keys = ((1, 2, 3),
+keys = [(1, 2, 3),
         (4, 5, 6),
         (7, 8, 9),
-        ('*', 0, '#'))
+        ('*', 0, '#')]
 
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
